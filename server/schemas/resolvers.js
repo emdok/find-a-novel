d22e06fb9e504fb8774ce2f1f49754bfc67e8ea4
Emily Dokken <dokkenemily@gmail.com>
2022-04-06 19:25:59 -0700

    add removeBook resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,34 +1,33 @@
-const { User, Book } = require('../models');
-const { signToken } = require('../utils/auth');
-const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id })
-        .select('-__V -password')
-        .populate('savedBooks')
+          .select("-__V -password")
+          .populate("savedBooks");
 
         return userData;
       }
 
-      throw new AuthenticationError('Not logged in!');
-    }
-    
+      throw new AuthenticationError("Not logged in!");
+    },
   },
   Mutation: {
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
@@ -51,10 +50,21 @@ const resolvers = {
       }
 
       throw new AuthenticationError("You need to be logged in!");
-      }
-      
     },
-    removeBook: async (parent, )
-  }
-}
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const user = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: bookId } },
+          { new: true }
+        );
+        return user;
+      }
+      throw new AuthenticationError(
+        "You need to be logged in to remove a book!"
+      );
+    },
+  },
+};
 
+module.exports = resolvers;
